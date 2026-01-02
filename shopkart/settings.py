5f56ffd7adac82c29a834bd7a2ddd71989c47cb8
Sakthisiddharth <sakthisiddharth90800@gmail.com>
2026-01-02 16:55:19 +0530

    all fix

--- shopkart/settings.py
+++ shopkart/settings.py
@@ -77,12 +77,8 @@ WSGI_APPLICATION = 'shopkart.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME' : 'shopkart',
-        'HOST' : 'localhost',
-        'USER' : 'root',
-        'PASSWORD' : 'BJsakthi@9704',
-        'PORT' : '3306'
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
